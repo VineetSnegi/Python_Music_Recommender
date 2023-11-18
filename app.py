3036199aaaf0b0f009ca55f17d0258fad79beb16
Vineet Singh Negi <137212462+VineetSnegi@users.noreply.github.com>
2023-11-18 23:35:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
-import pandas as pd
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 import streamlit as st
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import requests
 
+from streamlit_lottie import st_lottie
+
+# Getting Client Id and Client Secret
+client_id = '9024429ddca6460c8b6fc35343a37b00'
+client_secret = '8018a7729ac7408280c46250636fa216'
+auth_manage = SpotifyClientCredentials(client_id = client_id, client_secret = client_secret)
+sp = spotipy.Spotify(client_credentials_manager = auth_manage) # Spotify object to access API
+
+# Uploading image
+def load_musicurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+# Taking song name as input
+def get_recommendations(track_name):
+    # Getring track URI
+    results = sp.search(q = track_name, type = "track")
+    track_uri = results["tracks"]["items"][0]["uri"]
+
+    # Get recommended tracks
+    recommendations = sp.recommendations(seed_tracks = [track_uri], limit = 10)["tracks"]
+    return recommendations
 
 def get_analysis(start_year, end_year):
     st.markdown("""## Some Analysis of the year range are:
@@ -86,14 +113,14 @@ def get_analysis(start_year, end_year):
     # Add in title and subtitle
     ax.set_title('Hmm...')
     ax.text(x=.08, y=.86, 
-            s="",
-            transform=fig.transFigure,
-            ha='left',
-            fontsize=20,
+            s="", 
+            transform=fig.transFigure, 
+            ha='left', 
+            fontsize=20, 
             alpha=.8)
 
     # Label the lines directly
-    ax.text(x=.7, y=.795, s="""Energy""",
+    ax.text(x=.7, y=.795, s="""Energy""", 
             transform=fig.transFigure, ha='left', fontsize=20, alpha=.7)
     ax.text(x=.685, y=.61, s="""Danceability""", 
             transform=fig.transFigure, ha='left', fontsize=20, alpha=.7)
@@ -145,38 +172,85 @@ def get_analysis(start_year, end_year):
     col6.pyplot(fig)
 
 
-##==================
-## Streamlit Code
-##==================
-st.markdown("""# What songs were popular when I was in high school?
-The algorithm doesn't get you, we get that a lot. Maybe you want to rediscover the top songs from your high school days. Or maybe you just don't want to mess with making your own playlist. 
+# Creating Streamlit App
+def main():
 
-You can use this tool to find a pre-generated playlist of every song that made the Top 10 in the US for the years you select. 
-""")
+    ##==============================
+    ## Get Song Recommendation
+    ##==============================
+    
+    lottie_music = load_musicurl("https://lottie.host/96ee915d-6c68-4989-9415-49c4396a1a4a/eH1LnsMXmA.json")
+    st_lottie(lottie_music, speed=1, height=200, key="initial")
 
-df = pd.read_csv("playlists.csv")
-years = list(range(1958, 2022))
+    row0_1, row0_2= st.columns((2, 1))
+    row0_1.title("Music Recommendation System")
 
-year_range = st.slider(label="Start Year", 
-                       min_value=1958, 
-                       max_value=2022, 
-                       value=(1995, 2010))
+    row0_2.subheader(
+        "A music recommendation web app by Vineet Singh Negi"
+        )
 
-if st.button('Get Playlist'):
-    if (int(year_range[0]) - int(year_range[1])) == 0:
-        playlist_name = f"Top Singles: {year_range[0]}"
-    else:
-        playlist_name = f"Top Singles: {year_range[0]} - {year_range[1]}"
+    st.markdown(
+        "The algorithm doesn't get you, we get that a lot. Maybe you want to rediscover\
+        some new songs as per your music taste. Or maybe you you want top songs from school days and\
+        don't want to mess with making your own playlist."
+    )
+    st.markdown(
+        "**TO GET SONG RECOMMENDATION.** 👇"
+        )
 
-    if df[df['name'] == playlist_name].shape[0] > 0:
-        playlist = df[df['name'] == playlist_name].to_dict(orient='records')[0]
-    else:
-        playlist = "Ooops, it looks like we didn't make that playlist yet. Playlists with a **range of 1-20 years** were created. Try again with a more narrow year range."
+    track_name = st.text_input("Enter the song name:")
 
-    if isinstance(playlist, dict):
-        link = f"#### Your Spotify Playlist: [{playlist['name']}]({playlist['link']})"
-        st.markdown(link, unsafe_allow_html=True)
-        get_analysis(year_range[0], year_range[1])
+    if st.button("Get Recommendations"):
+        recommendations = get_recommendations(track_name)
+        
+        if recommendations is not None:
+            st.write("Recommended Songs Are:")
+            for track in recommendations:
+                st.write(track["name"])
+                track_image = track["album"]["images"][0]["url"]
+                st.image(track_image, width = 60)
 
-    else:
-        st.markdown(playlist)
+        else:
+            st.error("No such song exists")
+
+
+    st.markdown("")
+    st.markdown("")
+
+    ##==============================
+    ## Get Top Singles Playlist
+    ##==============================
+    st.markdown("**You can use this tool to find a pre-generated playlist of English songs that made\
+                the Top 10 for the years you select.**")
+
+    df = pd.read_csv("playlists.csv")
+    years = list(range(1958, 2022))
+
+    year_range = st.slider(label="Start Year", 
+                        min_value=1958, 
+                        max_value=2022, 
+                        value=(1995, 2010))
+
+
+    if st.button('Get Playlist'):
+        if (int(year_range[0]) - int(year_range[1])) == 0:
+            playlist_name = f"Top Singles: {year_range[0]}"
+        else:
+            playlist_name = f"Top Singles: {year_range[0]} - {year_range[1]}"
+
+        if df[df['name'] == playlist_name].shape[0] > 0:
+            playlist = df[df['name'] == playlist_name].to_dict(orient='records')[0]
+        else:
+            playlist = "Ooops, it looks like we didn't make that playlist yet. Playlists with a range of 1-20 years were created. Try again with a more narrow year range."
+
+        if isinstance(playlist, dict):
+            link = f"#### Your Spotify Playlist: [{playlist['name']}]({playlist['link']})"
+            st.markdown(link, unsafe_allow_html=True)
+            get_analysis(year_range[0], year_range[1])
+
+        else:
+            st.markdown(playlist)
+
+
+if __name__ == "__main__":
+    main()
